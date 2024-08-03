4fc84ae806fcfab5733be31a67ab9888db6a77ef
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2024-08-04 01:56:55 +0200

    initialize server and logic for script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+async function getData() {
+  const response = await fetch(
+    'https://apis.codante.io/olympic-games/countries'
+  );
+  const medals = await response.json();
+  return medals;
+}
