aebb27a306ad00e4c514e11a70ec1a5d0ad78b10
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2024-08-27 12:31:13 +0200

    Modified logic to handle query and changed styling on how query is displayed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-getData();
+handleData();
+
+const goldFlagEl = document.querySelector('.gold-TM');
+const silverFlagEl = document.querySelector('.silver-TM');
+const bronzeFlagEl = document.querySelector('.bronze-TM');
+
+const goldCountEl = document.querySelector('.gold-TMC');
+const silverCountEl = document.querySelector('.silver-TMC');
+const bronzeCountEl = document.querySelector('.bronze-TMC');
+
+const queryCountry = document.querySelector('.query-country');
+const queryMedal = document.querySelector('.query-medal');
+
+const searchField = document.querySelector('#search-field');
+const searchBtn = document.querySelector('#search-btn');
 
 async function getData() {
-  const goldFlagEl = document.querySelector('.gold-TM');
-  const silverFlagEl = document.querySelector('.silver-TM');
-  const bronzeFlagEl = document.querySelector('.bronze-TM');
-
-  const goldCountEl = document.querySelector('.gold-TMC');
-  const silverCountEl = document.querySelector('.silver-TMC');
-  const bronzeCountEl = document.querySelector('.bronze-TMC');
   try {
     const res = await fetch('https://apis.codante.io/olympic-games/countries');
     const countries = await res.json();
@@ -21,9 +28,20 @@ async function getData() {
     goldCountEl.textContent = data[0].total_medals;
     silverCountEl.textContent = data[1].total_medals;
     bronzeCountEl.textContent = data[2].total_medals;
+
     return data;
   } catch {
     const err = 'Error: Could not retrieve data';
     console.log(err);
   }
+}
+
+async function handleData() {
+  const data = await getData();
+  searchBtn.addEventListener('click', () => {
+    const input = searchField.value;
+    const foundObject = data.find((item) => item.id === input);
+    queryCountry.src = foundObject.flag_url;
+    queryMedal.textContent = `Total medals: ${foundObject.total_medals}`;
+  });
 }
