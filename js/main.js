7875104525c947365d089851aa717e0f96b15ec6
RjayBrown <rjbrown.dev@outlook.com>
2024-08-30 09:47:40 -0400

    refactored classes/ids

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 handleData();
 
-const goldFlagEl = document.querySelector('.gold-TM');
-const silverFlagEl = document.querySelector('.silver-TM');
-const bronzeFlagEl = document.querySelector('.bronze-TM');
+const goldFlagEl = document.querySelector('.gold-flag');
+const silverFlagEl = document.querySelector('.silver-flag');
+const bronzeFlagEl = document.querySelector('.bronze-flag');
 
 const goldCountEl = document.querySelector('.gold-TMC');
 const silverCountEl = document.querySelector('.silver-TMC');
 const bronzeCountEl = document.querySelector('.bronze-TMC');
 
 const queryCountry = document.querySelector('.query-country');
-const queryMedal = document.querySelector('.query-medal');
+const queryBronze = document.querySelector('.query-bronze');
+const querySilver = document.querySelector('.query-silver');
+const queryGold = document.querySelector('.query-gold');
+const queryTotal = document.querySelector('.query-total-medal');
 
 const searchField = document.querySelector('#search-field');
 const searchBtn = document.querySelector('#search-btn');
@@ -20,18 +23,19 @@ async function getData() {
     const countries = await res.json();
     const data = countries.data;
     data.sort((a, b) => b.total_medals - a.total_medals);
+    console.log(data)
 
     goldFlagEl.src = data[0].flag_url;
     silverFlagEl.src = data[1].flag_url;
     bronzeFlagEl.src = data[2].flag_url;
 
-    goldCountEl.textContent = data[0].total_medals;
-    silverCountEl.textContent = data[1].total_medals;
-    bronzeCountEl.textContent = data[2].total_medals;
+    // goldCountEl.textContent = data[0].total_medals;
+    // silverCountEl.textContent = data[1].total_medals;
+    // bronzeCountEl.textContent = data[2].total_medals;
 
     return data;
   } catch {
-    const err = 'Error: Could not retrieve data';
+    const err = 'Error: Could not retrieve data!';
     console.log(err);
   }
 }
@@ -43,7 +47,7 @@ async function handleData() {
     const foundObject = data.find((item) => item.id === input);
     queryCountry.scrollIntoView({ behavior: 'smooth' });
     queryCountry.src = foundObject.flag_url;
-    queryMedal.textContent = `Total medals: ${foundObject.total_medals}`;
+    queryTotal.textContent = `Total medals: ${foundObject.total_medals}`;
     input.value = '';
   });
 }
